a73b21ce470cc901db87d93fe4bf76b87b0c47ce
NexushasTaken <nexushastaken@gmail.com>
2023-06-15 01:00:14 +0800

    Better nvim-lint configuration.

--- lua/nexus/lazy-config/nvim-lint.lua
+++ lua/nexus/lazy-config/nvim-lint.lua
@@ -1,12 +1,53 @@
 local lint = require'lint'
+local cmd = vim.cmd
+local api = vim.api
+local create_cmd = api.nvim_create_user_command
+local glint = api.nvim_create_augroup('nvim-lint', { clear = true })
+
+-- Linter enable/disable
+local enabled = false
 
 lint.linters_by_ft = {
   c   = { 'cpplint' },
   cpp = { 'cpplint' },
 }
 
-vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
-  callback = function()
-    require'lint'.try_lint()
-  end
-})
+lint.linters.cpplint.args = {
+  '--filter=-legal/copyright',
+}
+
+create_cmd('Lint', function() lint.try_lint() end, { nargs = 0 })
+
+create_cmd('LintEnable', function()
+    if enabled then
+      print('Linter is already enabled')
+    else
+      enabled = true
+      api.nvim_create_autocmd({ 'BufWritePost' }, {
+        group = glint,
+        callback = function() cmd.Lint() end,
+      })
+      cmd.Lint()
+      print('Linter Enabled')
+    end
+  end, { nargs = 0 })
+
+create_cmd('LintDisable', function()
+    if enabled then
+      enabled = false
+
+      vim.diagnostic.hide()
+      api.nvim_clear_autocmds({ group = glint })
+      print('Linter Disabled')
+    else
+      print('Linter is already disabled')
+    end
+  end, { nargs = 0 })
+
+create_cmd('LintToggle', function()
+    if enabled then
+      cmd.LintDisable()
+    else
+      cmd.LintEnable()
+    end
+  end, { nargs = 0 })
