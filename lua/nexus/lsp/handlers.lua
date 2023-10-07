4c4ae6f352277cfd38e1ce2d00f6c63d7a93952b
NexushasTaken <nexushastaken@gmail.com>
2023-10-07 16:45:46 +0800

    added and change keybinds

--- lua/nexus/lsp/handlers.lua
+++ lua/nexus/lsp/handlers.lua
@@ -58,17 +58,22 @@ end
 local function lsp_keymaps(bufnr)
   local opts = { noremap = true, silent = true, };
   local map = vim.api.nvim_buf_set_keymap;
-  map(bufnr, "n", "<leader>ge", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts);
-  map(bufnr, "n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts);
-  map(bufnr, "n", "<leader>K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts);
-  map(bufnr, "n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts);
-  map(bufnr, "n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts);
+  map(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts);
+  map(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts);
+  map(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts);
+  map(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts);
+  map(bufnr, "n", "<leader>li", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts);
   map(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts);
-  map(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts);
   map(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts);
   map(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts);
   map(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts);
   map(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts);
+  map(bufnr, "n", "<leader>lh", "<cmd>lua vim.lsp.buf.document_highlight()<CR>", opts);
+  map(bufnr, "n", "<leader>lt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts);
+  vim.api.nvim_create_autocmd({"CursorMoved"}, {
+    buffer = bufnr,
+    command = "lua vim.lsp.buf.clear_references()",
+  })
 end
 
 local toSnakeCase = function(str)
