08167ee2cd5623ab7a622113a946ca7eaa924db3
NexushasTaken <nexushastaken@gmail.com>
2024-08-08 23:21:53 +0800

    add c3-lang treesitter

--- lua/nexus/lazy-config/treesitter.lua
+++ lua/nexus/lazy-config/treesitter.lua
@@ -13,3 +13,22 @@ configs.setup({
     enable_close_on_slash = false,
   },
 });
+
+-- # c3 language
+vim.filetype.add({
+  extension = {
+    c3 = "c3",
+    c3i = "c3",
+    c3t = "c3",
+  },
+});
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs();
+parser_config.c3 = {
+  install_info = {
+    url = "https://github.com/c3lang/tree-sitter-c3",
+    files = {"src/parser.c", "src/scanner.c"},
+    branch = "main",
+  },
+};
+-- > c3 language
