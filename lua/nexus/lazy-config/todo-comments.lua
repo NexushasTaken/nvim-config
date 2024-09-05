8a4463ed7cb67e81cce9bd52f200a69337759b13
NexushasTaken <nexushastaken@gmail.com>
2024-09-06 00:08:16 +0800

    change todo-comment patterns

--- lua/nexus/lazy-config/todo-comments.lua
+++ lua/nexus/lazy-config/todo-comments.lua
@@ -27,12 +27,12 @@ return {
   -- * after: highlights after the keyword (todo text)
   highlight = {
     multiline = true, -- enable multine todo comments
-    multiline_pattern = [[^.*:]], -- lua pattern to match the next multiline from the start of the matched keyword
+    multiline_pattern = [[^.*: ]], -- lua pattern to match the next multiline from the start of the matched keyword
     multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
     before = "", -- "fg" or "bg" or empty
     keyword = "fg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
     after = "fg", -- "fg" or "bg" or empty
-    pattern = [[\s+(KEYWORDS):]], -- pattern or table of patterns, used for highlighting (vim regex)
+    pattern = [[\s+(KEYWORDS): ]], -- pattern or table of patterns, used for highlighting (vim regex)
     comments_only = true, -- uses treesitter to match keywords in comments only
     max_line_len = 400, -- ignore lines longer than this
     exclude = {}, -- list of file types to exclude highlighting
