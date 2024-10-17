1ff3adbdebad7c12ce786db20214ab1c63e641ce
NexushasTaken <nexushastaken@gmail.com>
2024-10-17 20:01:57 +0800

    enable netrw plugins

--- lua/nexus/plugins.lua
+++ lua/nexus/plugins.lua
@@ -21,7 +21,7 @@ local opts = {
     rtp = {
       disabled_plugins = {
         "2html_plugin", "tohtml", "getscript", "getscriptPlugin", "gzip",
-        "logipat", "netrw", "netrwPlugin", "netrwSettings", "netrwFileHandlers",
+        "logipat", --[[ "netrw", "netrwPlugin", "netrwSettings", "netrwFileHandlers", ]]
         "matchit", "tar", "tarPlugin", "rrhelper", "spellfile_plugin", "vimball",
         "vimballPlugin", "zip", "zipPlugin", "tutor", "rplugin", "syntax",
         "synmenu", "optwin", "compiler", "bugreport", "ftplugin", "spellfile"
