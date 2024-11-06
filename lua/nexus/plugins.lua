91fdf8e0ca73e9318e30516357d60eab02d5b3bf
NexushasTaken <nexushastaken@gmail.com>
2024-11-06 19:56:59 +0800

    add orgmode plugin

--- lua/nexus/plugins.lua
+++ lua/nexus/plugins.lua
@@ -321,6 +321,19 @@ lazy.setup({
     config = load_config("hardtime"),
   },
 
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      -- Setup orgmode
+      require("orgmode").setup({
+        org_agenda_files = "~/orgfiles/**/*",
+        org_default_notes_file = "~/orgfiles/refile.org",
+      });
+    end,
+  },
+
   { -- Debugger
     "rcarriga/nvim-dap-ui",
     keys = {
