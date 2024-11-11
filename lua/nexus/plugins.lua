cd32ff84f88e5cf26ecbeefed2e98ad462ef21df
NexushasTaken <nexushastaken@gmail.com>
2024-11-11 23:18:49 +0800

    add render-markdown plugin

--- lua/nexus/plugins.lua
+++ lua/nexus/plugins.lua
@@ -328,6 +328,18 @@ lazy.setup({
     config = load_config("orgmode"),
   },
 
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "echasnovski/mini.nvim"
+    }, -- if you use the mini.nvim suite
+    ---@module "render-markdown"
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
   { -- Debugger
     "rcarriga/nvim-dap-ui",
     keys = {
