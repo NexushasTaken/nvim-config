a73a8ff1fd4817091370aca0620eed77a561ea18
NexushasTaken <nexushastaken@gmail.com>
2024-11-18 16:20:38 +0800

    add neogit plugin

--- lua/nexus/plugins.lua
+++ lua/nexus/plugins.lua
@@ -385,6 +385,21 @@ lazy.setup({
   },
 
   {
+    "NeogitOrg/neogit",
+    cmd = { "Neogit" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+      "echasnovski/mini.pick",         -- optional
+    },
+    config = load_config("neogit"),
+  },
+
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require("nexus.lsp");
