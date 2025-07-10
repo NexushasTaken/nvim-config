7f90d623ca42bbd28b33c11f25a69c463201a23c
NexushasTaken <nexushastaken@gmail.com>
2025-07-11 06:49:49 +0800

    use mini.statusline as a replacement for lualine

--- lua/plugins/config/mini.lua
+++ lua/plugins/config/mini.lua
@@ -1,4 +1,41 @@
+function statusline()
+  local api = require("mini.statusline");
+  function active()
+    local mode, mode_hl = MiniStatusline.section_mode({ })
+    local git           = MiniStatusline.section_git({ })
+    local diff          = MiniStatusline.section_diff({ })
+    local diagnostics   = MiniStatusline.section_diagnostics({
+      signs = { ERROR = "󰅚 ", WARN = "󰀪 ", INFO = "󰋽 ", HINT = "󰌶 "
+    }});
+    local lsp           = MiniStatusline.section_lsp({ })
+    local filename      = MiniStatusline.section_filename({ })
+    local fileinfo      = MiniStatusline.section_fileinfo({ })
+    local location      = MiniStatusline.section_location({ })
+    local search        = MiniStatusline.section_searchcount({ })
+
+    return MiniStatusline.combine_groups({
+      { hl = mode_hl,                  strings = { mode } },
+      { hl = "MiniStatuslineDevinfo",  strings = { git, diff, diagnostics, lsp } },
+      "%<", -- Mark general truncate point
+      { hl = "MiniStatuslineFilename", strings = { filename } },
+      "%=", -- End left alignment
+      { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+      { hl = mode_hl,                  strings = { search, location } },
+    })
+  end
+
+  api.setup({
+    content = {
+      active = active,
+      inactive = nil,
+    },
+    use_icons = true,
+  });
+end
+
 return function()
+  statusline();
+
   local MiniDeps = require("mini.deps")
   --MiniDeps.add({
   --  source = "saghen/blink.cmp",
@@ -7,17 +44,17 @@ return function()
   --})
 
   --local function build_blink(params)
-  --  vim.notify('Building blink.cmp', vim.log.levels.INFO)
-  --  local obj = vim.system({ 'cargo', 'build', '--release' }, { cwd = params.path }):wait()
+  --  vim.notify("Building blink.cmp", vim.log.levels.INFO)
+  --  local obj = vim.system({ "cargo", "build", "--release" }, { cwd = params.path }):wait()
   --  if obj.code == 0 then
-  --    vim.notify('Building blink.cmp done', vim.log.levels.INFO)
+  --    vim.notify("Building blink.cmp done", vim.log.levels.INFO)
   --  else
-  --    vim.notify('Building blink.cmp failed', vim.log.levels.ERROR)
+  --    vim.notify("Building blink.cmp failed", vim.log.levels.ERROR)
   --  end
   --end
 
   --MiniDeps.add({
-  --  source = 'Saghen/blink.cmp',
+  --  source = "Saghen/blink.cmp",
   --  hooks = {
   --    post_install = build_blink,
   --    post_checkout = build_blink,
